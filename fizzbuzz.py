b024a150ff1c848b1151624517e8ccef2562801d
GalloDeOro <aruanoa1500@alumno.ipn.mx>
2024-08-26 05:51:32 -0700

    correccion gneral, trabajamos con vetores dados

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,21 +1,18 @@
 import numpy as np 
 
 def fizzbuzz(x):
+    y = []
     for i in x:
-        i=int(i)
-        if(i%3 == 0):
-            print('Fizz')
-        if(i%5 == 0): 
-            print('buzz')
-        if(i%3 == 0  and i%5 == 0 ):
-            print('Fizzbuzz')
+        if i%3 ==0 and i%5 ==0:
+            y.append("fizzbuzz")
+        elif i%3 == 0:
+            y.append("fizz")
+        elif i%5 == 0: 
+            y.append('buzz')
         else:
-            print(i)
+            y.append(i)
+    return y
 
 
-n=input('Introduzca cuantos numero se van a evaluar: ')
-x = []
-for j in range(int(n)):
-    x.append(input())
-
-print(fizzbuzz(x))
+vinicial = [1,2,3,4,5,15]
+print(fizzbuzz(vinicial))
